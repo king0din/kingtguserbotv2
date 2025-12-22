20aa5c5820ead8e33c9b1b005f0492529a70006b
king odin <76708652+king0din@users.noreply.github.com>
2025-12-22 21:23:33 +0300

    ❤️

--- modules/selam.py
+++ modules/selam.py
@@ -1,12 +1,10 @@
 from telethon import events
 import asyncio
 
-# Modül içindeki komut: .sa
+# @events.register kullandığında, main.py içindeki "isinstance" bunu tanır.
 @events.register(events.NewMessage(outgoing=True, pattern=r'\.sa'))
 async def selam_animasyon(event):
     animasyon = ["S", "Se", "Sel", "Selam", "Selam Aleyküm!", "🌹"]
     for kare in animasyon:
         await event.edit(kare)
         await asyncio.sleep(0.5)
-
-# Not: main.py'deki yükleme mekanizmasının bunu client'a kaydetmesi gerekir.
